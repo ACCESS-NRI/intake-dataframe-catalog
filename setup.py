df045b4df2a40dcc6e5685d9ea2bbafad7359117
dougiesquire <dougiesquire@gmail.com>
2023-03-23 09:14:57 +1100

    rename package

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@ from setuptools import find_packages, setup
 import versioneer
 
 setup(
-    name="intake_meta_esm",
+    name="intake_dataframe_catalog",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author="Dougie Squire",
+    author="ACCESS-NRI",
+    url="https://github.com/ACCESS-NRI/intake-dataframe-catalog",
+    description="An intake driver for a searchable table of intake catalogs and associated metadata",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
     install_requires=[
         "intake",
-        "intake-esm",
     ],
     entry_points={
         "intake.drivers": [
-            "meta_esm_datastore = intake_meta_esm.core:meta_esm_datastore",
+            "df_catalog = intake_dataframe_catalog.core:df_catalog",
         ]
     },
 )
