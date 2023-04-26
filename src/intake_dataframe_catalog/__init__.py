72389145ffe77f3ba22a8c8b9f3548569e71b962
dougiesquire <dougiesquire@gmail.com>
2023-04-26 12:37:07 +1000

    remove DFCatalogModel

--- src/intake_dataframe_catalog/__init__.py
+++ src/intake_dataframe_catalog/__init__.py
@@ -6,11 +6,3 @@ import intake
 from . import _version
 
 __version__ = _version.get_versions()["version"]
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
